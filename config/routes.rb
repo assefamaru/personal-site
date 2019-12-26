6e513718d660cd7c2ac7a8175d7797aa0c56045f
Alexander Maru <i@alexandermaru.com>
2019-12-26 13:13:16 -0500

    Bump to rails 6.0 and ruby 2.7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-	root 'static#home'
-	get  'about',   to: 'static#about'
-	get  'contact', to: 'static#contact'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
